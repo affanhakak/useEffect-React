fc57502c25a10800dcdbc53539fe90c605e3b64c
Affan Hakak <affanshowkat@gmail.com>
2024-07-23 12:38:11 +0530

    added loader

--- src/QuoteFetcher.jsx
+++ src/QuoteFetcher.jsx
@@ -3,6 +3,7 @@ const Random_Quote_Url = "https://inspo-quotes-api.herokuapp.com/quotes/random";
 
 function QuoteFetcher() {
   const [quote, setQuote] = useState({ text: "", author: "" });
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetchQuote();
   }, []);
@@ -11,9 +12,14 @@ function QuoteFetcher() {
     const jsonResponse = await response.json();
     const randomQuote = jsonResponse.quote;
     setQuote(randomQuote);
+    setIsLoading(false);
   }
   return (
     <div>
+      <p className="Loader" style={{ opacity: isLoading ? 1 : 0 }}>
+        Loading...
+      </p>
+
       <h2>{quote.text}</h2>
       <h3>{quote.author}</h3>
       <button onClick={fetchQuote}>Get Quote using handler</button>
